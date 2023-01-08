ae82b495ca5963a43eae61c17270e6cb4b645959
Rhys Bowen Harries <rhys@optimiseweb.co.uk>
2023-01-08 16:26:47 +0000

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,72 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useFonts } from "expo-font";
+
+export default function Header() {
+  const [loaded] = useFonts({
+    regular: require("../../assets/font/BrandonGrotesque-Black.ttf"),
+    bold: require("../../assets/font/BrandonGrotesque-Bold.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image
+          style={styles.image}
+          source={require("../../assets/img/spacex-logo.png")}
+        />
+        <Text style={styles.iconText}>LAUNCHES</Text>
+      </View>
+      <TouchableOpacity>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Reload Data</Text>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/icon/refresh.png")}
+          />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 60,
+    marginLeft: 25,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: 160,
+    height: 20,
+  },
+  iconText: {
+    fontFamily: "regular",
+    color: "#797979",
+    fontSize: 18,
+  },
+  buttonContainer: {
+    paddingHorizontal: 20,
+    width: 150,
+    height: 50,
+    backgroundColor: "#215184",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomLeftRadius: 30,
+    borderTopLeftRadius: 30,
+  },
+  buttonText: {
+    fontFamily: "bold",
+    color: "white",
+    fontSize: 17,
+  },
+  icon: {
+    width: 15,
+    height: 15,
+  },
+});
